ee3a2670765be977d66e9b69ac5cd4d53f9b47b3
piyumiyashodhara <piyumiyasodhara@gmail.com>
2026-03-20 22:54:16 +0530

    Modified the repository and servise files

--- petcarehub/src/main/java/com/petcarehub/medicalRecords/repository/MedicalTreatmentRepository.java
+++ petcarehub/src/main/java/com/petcarehub/medicalRecords/repository/MedicalTreatmentRepository.java
@@ -1,4 +1,12 @@
 package com.petcarehub.medicalRecords.repository;
 
-public class MedicalTreatmentRepository {
+import com.petcarehub.medicalRecords.entity.MedicalTreatment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MedicalTreatmentRepository extends JpaRepository<MedicalTreatment, Long> {
+    List<MedicalTreatment> findByPetPetIdOrderByTreatmentDateDesc(Long petId);
 }
